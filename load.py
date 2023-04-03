e1310caade9120680ca2c8efd29445feb88e0a96
masdaster <118899802+masdaster@users.noreply.github.com>
2023-04-03 08:12:43 +0200

    Remove russian translation.

--- load.py
+++ load.py
@@ -1,7 +1,5 @@
 import tkinter as tk
 import logging
-import l10n
-import functools
 import os
 
 from typing import Optional, Tuple, Dict, Any
@@ -9,8 +7,6 @@ from config import appname
 
 plugin_name = os.path.basename(os.path.dirname(__file__))
 logger = logging.getLogger(f'{appname}.{plugin_name}')
-
-_ = functools.partial(l10n.Translations.translate, context=__file__)
 
 label: Optional[tk.Label]
 status: Optional[tk.Label]
@@ -48,13 +44,13 @@ def dashboard_entry(cmdr: str, is_beta: bool, entry: Dict[str, Any]) -> None:
 
 def update_status() -> None:
   global label, status
-  label["text"] = f'{_("Fuel levels")}:'
+  label["text"] = "Fuel levels"
   if main_tank is None or reservoir is None:
     if main_tank is None and reservoir is None:
-      status["text"] = _("waiting for data …")
+      status["text"] = "waiting for data …"
     else:
       # maybe add error handling for this weird edge case, should it ever exist …
-      status["text"] = _("ERROR")
+      status["text"] = "ERROR"
       logger.error("One of main tank and reservoir fuel levels is None, the other isn’t … WTF?")
   else:
-    status["text"] = f'{round(main_tank,3)} t ({_("main")}), {round(reservoir,3)} t ({_("reservoir")})'
+    status["text"] = f'{round(main_tank,3)} t (main), {round(reservoir,3)} t (reservoir)'
